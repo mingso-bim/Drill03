66e8dfce810a8afe87bc8f10f55199e8df86d8cf
mingso <kimdud0@naver.com>
2024-09-24 11:49:57 +0900

    뼈대 완성(무한루프 확인)

--- character_grass.py
+++ character_grass.py
@@ -3,14 +3,25 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+boy = load_image('character.png')
 
-x = 0
-while (x < 800):
-    clear_canvas_now()    
-    grass.draw_now(400, 30)
-    character.draw_now(x, 90)
-    x = x + 2
-    delay(0.01)
+def run_circle():
+    print('circle')
+    
+
+def run_rectangle():
+    print('rectangle')
+
+
+
+
+
+    
+while True:
+    run_circle()
+    run_rectangle()
+
+
+    
 
 close_canvas()
